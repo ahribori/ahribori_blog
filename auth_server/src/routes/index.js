d565d3a0350cf9b9daa89931f67ab94a7740be2d
Hyun Seung Jeong <hs9923@naver.com>
2017-01-11 17:41:26 +0900

    - npm 실행 스크립트 서버 경로 수정

--- auth_server/src/routes/index.js
+++ auth_server/src/routes/index.js
@@ -4,6 +4,13 @@ import auth from './auth';
 import user from './user';
 import authMiddleware from '../middlewares/auth';
 
+router.get('/', (req, res) => {
+	res.json({
+		success: true,
+		message: 'ahribori authorization server'
+	});
+});
+
 router.use('/auth', auth);
 router.use('/user', authMiddleware);
 router.use('/user', user);
