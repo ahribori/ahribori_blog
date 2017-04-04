be75eecd7433a678bcfdb664c9c434d1ed1004c7
Hyun Seung Jeong <ahribori@gmail.com>
2017-04-04 13:50:45 +0900

    - 카카오링크, 카카오스토리 공유 추가

--- client/src/components/article/Share.js
+++ client/src/components/article/Share.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import {
     ShareButtons,
     ShareCounts,
@@ -8,14 +9,12 @@ import {
 const {
     FacebookShareButton,
     GooglePlusShareButton,
-    LinkedinShareButton,
     TwitterShareButton,
 } = ShareButtons;
 
 const {
     FacebookShareCount,
     GooglePlusShareCount,
-    LinkedinShareCount,
 } = ShareCounts;
 
 const FacebookIcon = generateShareIcon('facebook');
@@ -28,6 +27,43 @@ class Share extends React.Component {
     constructor(props) {
         super(props);
         this.state = { ... props }
+        this.shareKakaoStory = this.shareKakaoStory.bind(this);
+    }
+
+    shareKakaoStory() {
+        Kakao.Story.share({
+            url: this.state.url,
+            text: this.state.title
+        })
+    }
+
+    componentDidMount() {
+        if (!document.getElementById('kakao-sdk')) {
+            ((d, s, id, cb) => {
+                const element = d.getElementsByTagName(s)[0];
+                const fjs = element;
+                let js = element;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = '//developers.kakao.com/sdk/js/kakao.min.js';
+                fjs.parentNode.insertBefore(js, fjs);
+                js.onload = cb;
+            })(document, 'script', 'kakao-sdk', () => {
+                Kakao.init(process.env.KAKAO_KEY);
+                Kakao.Link.createDefaultButton({
+                    container: '#kakao_link_btn',
+                    objectType: 'feed',
+                    content: {
+                        title: this.state.title,
+                        imageUrl: this.state.picture,
+                        link: {
+                            webUrl: this.state.url
+                        },
+                        description: this.state.description
+                    }
+                });
+            });
+        }
     }
 
     render() {
@@ -35,24 +71,29 @@ class Share extends React.Component {
             <div className="share_btn_group">
                 <span className="share_btn_group_title">이 글 공유하기</span>
                 <div className="article_share_network">
+                    <div id="kakao_link_btn" className="kakao_share_btn">
+                        <img src="/image/kakao_icon.png" height={32} width={32}/>
+                    </div>
+                </div>
+                <div className="article_share_network">
+                    <div id="kakaostory_link_btn" className="kakao_share_btn" onTouchTap={this.shareKakaoStory}>
+                        <img src="/image/kakaostory_icon.png" height={32} width={32}/>
+                    </div>
+                </div>
+                <div className="article_share_network">
                     <FacebookShareButton url={this.state.url} description={this.state.description} title={this.state.title} picture={this.state.picture}>
                         <FacebookIcon size={32} round />
                     </FacebookShareButton>
                 </div>
                 <div className="article_share_network">
-                    <GooglePlusShareButton url={this.state.url}>
-                        <GooglePlusIcon size={32} round />
-                    </GooglePlusShareButton>
-                </div>
-                <div className="article_share_network">
-                    <LinkedinShareButton url={this.state.url} description={this.state.description} title={this.state.title}>
-                        <LinkedinIcon size={32} round />
-                    </LinkedinShareButton>
-                </div>
-                <div className="article_share_network">
                     <TwitterShareButton url={this.state.url} title={this.state.title}>
                         <TwitterIcon size={32} round />
                     </TwitterShareButton>
+                </div>
+                <div className="article_share_network">
+                    <GooglePlusShareButton url={this.state.url}>
+                        <GooglePlusIcon size={32} round />
+                    </GooglePlusShareButton>
                 </div>
             </div>
         );
