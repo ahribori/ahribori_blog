d76f4fda53c661e6e3cbd8f13fa388ce957f6e09
Hyun Seung Jeong <ahribori@gmail.com>
2017-04-10 15:34:25 +0900

    ckeditor 이미지업로드 request hook

--- client/src/components/article/CKEditor.js
+++ client/src/components/article/CKEditor.js
@@ -27,8 +27,22 @@ export default class CKEditor extends Component {
 			}
         }.bind(this));
 
-		CKEDITOR.instances[this.elementName].on( 'fileUploadRequest', function( evt ) {
-		});
+        CKEDITOR.instances[this.elementName].on('fileUploadRequest', function (evt) {
+            const fileLoader = evt.data.fileLoader;
+            const formData = new FormData();
+            const xhr = fileLoader.xhr;
+            const mode = localStorage.getItem("editor_mode");
+
+            xhr.open('POST', fileLoader.uploadUrl, true);
+            formData.append('upload', fileLoader.file, fileLoader.fileName);
+            formData.append('mode', mode);
+            (mode === 'modify') ?
+                formData.append("article_id", localStorage.getItem("article_id")) :
+                formData.append("article_temp_id", localStorage.getItem("article_temp_id"));
+
+            fileLoader.xhr.send(formData);
+            evt.stop();
+        });
 
 		CKEDITOR.instances[this.elementName].on( 'fileUploadResponse', function( evt ) {
 		} );
