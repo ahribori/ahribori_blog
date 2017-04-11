2beb8454ec5c0e688af601877aaf3bc3ac1fbf96
Hyun Seung Jeong <ahribori@gmail.com>
2017-04-11 09:44:44 +0900

    권한이 없을 떄 article 데이터 reject

--- api_server/src/routes/article.js
+++ api_server/src/routes/article.js
@@ -335,11 +335,13 @@ router.get('/:id', (req, res) => {
 					message: 'article not found'
 				})
 			} else {
-				if (!req.payload._id || (req.payload._id !== article.author_id)) {
-					reject({
-						status: 403,
-						message: 'unauthorized'
-					})
+				if (article.hidden === true) {
+					if (!req.payload._id || (req.payload._id !== article.author_id)) {
+						reject({
+							status: 403,
+							message: 'unauthorized'
+						})
+					}
 				}
 				if (req.header('x-h') * 1 === 1) {
                 	article.hit++;
