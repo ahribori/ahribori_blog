91ec295f380c842fa507a8778d95798cfe1b5bfc
Hyun Seung Jeong <ahribori@gmail.com>
2017-04-28 12:30:40 +0900

    - create-react-app 기반 -> 대규모 마이그레이션

--- auth_server_legacy/src/middlewares/CORS.js
+++
@@ -1,8 +0,0 @@
-const CORS = (req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-	(req.method === 'OPTIONS') ? res.sendStatus(200) : next();
-};
-
-export default CORS;
